a6a7507214f0f52527f6896fb970096b004d451f
bene <matheusbenediht@gmail.com>
2023-08-18 16:43:19 -0300

    First commi

---
+++ src/main/java/com/mylibrary/MyLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.mylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibrarySystemApplication.class, args);
+	}
+
+}
